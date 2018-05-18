b420c6cb55c0dcd93b394c00751e04634eb5acab
msunkist99 <msunkist99@gmail.com>
2018-05-17 19:02:10 -0500

    Initial commit

---
+++ 20180124_1.2_NumbersStringsDatesTimes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180124_1._2_NumbersStringsDatesTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello Mary!");
+            Console.ReadLine();
+
+            var name = "Mary - using variable";
+            Console.WriteLine("Hello " + name + "!");
+            Console.ReadLine();
+
+            Console.WriteLine($"Hello {name} - using string interpolation");
+            Console.ReadLine();
+
+            Console.WriteLine($"Hello {name.ToUpper()} all UPPER case this time");
+            Console.ReadLine();
+
+            var names = new List<string> { "Mary", "Felipe", "Emillia" };
+            foreach (var individualName in names)
+            {
+                Console.WriteLine($"Hello {individualName} from foreach loop");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
